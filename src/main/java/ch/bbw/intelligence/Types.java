22b8f5ea046ddbe88d1faffd778991d0d488749e
Janbei16 <jannis.beichler@gmail.com>
2022-06-23 12:24:14 +0200

    Habe die Klasse Types gemacht :)

---
+++ src/main/java/ch/bbw/intelligence/Types.java
@@ -0,0 +1,19 @@
+package ch.bbw.intelligence;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Types {
+
+    int spacial_intelligence;
+    int body_Kinesthetic_intelligence;
+    int musical_intelligence;
+    int lingustic_intelligence;
+    int logical_Mathematical_skills;
+    int interpersonal_intelligence;
+    int intrapersonal_intelligence;
+    int naturalistic_intelligence;
+
+}
