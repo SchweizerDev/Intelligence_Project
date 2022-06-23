85805ab088863fff3e5d9ad2382f91dcfcbe53ef
VerucktrDev <lucadubi@gmail.com>
2022-06-23 11:48:27 +0200

    Initial commit

---
+++ src/main/java/ch/bbw/intelligence/IntelligenceProjectApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.intelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligenceProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligenceProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbw/intelligence/IntelligenceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.intelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligenceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
